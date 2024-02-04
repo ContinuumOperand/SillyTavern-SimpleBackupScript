4d5733dce324fbf213c8ae9cb1747331eb285847
Continuum Operand <145922748+ContinuumOperand@users.noreply.github.com>
2024-02-04 03:45:50 -0300

    Add Backup.bat

---
+++ Backup.bat
@@ -0,0 +1,22 @@
+7z a "%~dp0user-backups\backup_%date:~-4,4%%date:~-7,2%%date:~-10,2%_%time:~0,2%%time:~3,2%.7z" ^
+    "public\assets\*" ^
+    "public\Backgrounds\*" ^
+    "public\Characters\*" ^
+    "public\Chats\*" ^
+    "public\context\*" ^
+    "public\Group chats\*" ^
+    "public\Groups\*" ^
+    "public\instruct\*" ^
+    "public\KoboldAI Settings\*" ^
+    "public\movingUI\*" ^
+    "public\NovelAI Settings\*" ^
+    "public\OpenAI Settings\*" ^
+    "public\QuickReplies\*" ^
+    "public\TextGen Settings\*" ^
+    "public\themes\*" ^
+    "public\User Avatars\*" ^
+    "public\user\*" ^
+    "public\worlds\*" ^
+    "public\settings.json" ^
+    "secrets.json" ^
+    "config.yaml"
